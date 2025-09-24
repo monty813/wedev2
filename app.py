05f745758e12d4594ec5ae3d4129879435e1ae5d
monty813 <monty813@school.lu>
2025-09-24 11:08:22 +0200

    9/24

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/about") 
+def about(): 
+     return render_template("about.html") 
+ 
+@app.route('/submit', methods=["POST"]) 
+
+def submit(): 
+    username = request.form['username'] 
+    message = request.form['message'] 
+    return f"Thanks {username}, your message was: {message}" 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
